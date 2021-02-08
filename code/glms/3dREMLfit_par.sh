aa5f6c9c3d6e74b6b0cdd9b7b0103087940e4a86
mcfreund <m.freund@wustl.edu>
2021-02-08 17:41:55 -0600

    remlfit now serial

--- code/glms/3dREMLfit_par.sh
+++ code/glms/3dREMLfit_par.sh
@@ -6,37 +6,27 @@
 
 wd=$(pwd)
 
-function remlfit {
-	
-	
-	## define paths and names
-	dir_stimts=${stimts}${subject}/input/pro
-	dir_out=${out}${subject}/results/${glm}
-	name_img_run1=${img}${subject}/INPUT_DATA/Stroop/proactive/lpi_scale_blur4_tfMRI_StroopPro1_AP.nii.gz
-	name_img_run2=${img}${subject}/INPUT_DATA/Stroop/proactive/lpi_scale_blur4_tfMRI_StroopPro2_PA.nii.gz
+## define paths and names
+dir_stimts=${stimts}${subject}/input/pro
+dir_out=${out}${subject}/results/${glm}
+name_img_run1=${img}${subject}/INPUT_DATA/Stroop/proactive/lpi_scale_blur4_tfMRI_StroopPro1_AP.nii.gz
+name_img_run2=${img}${subject}/INPUT_DATA/Stroop/proactive/lpi_scale_blur4_tfMRI_StroopPro2_PA.nii.gz
 
-	cd ${dir_out}	
+cd ${dir_out}	
 
-	/usr/local/pkg/afni_18/3dREMLfit \
-	-matrix ${dir_out}/X.xmat.1D \
-	-input ${name_img_run1} ${name_img_run1} \
-	-Rvar ${dir_out}/stats_var_${subject}.nii.gz \
-	-Rbuck ${dir_out}/stats_${subject}.nii.gz \
-	-rwherr ${dir_out}/wherr_${subject}.nii.gz \
-	-rerrts ${dir_out}/errts_${subject}.nii.gz \
-	-GOFORIT \
-	-fout \
-	-tout \
-	-nobout \
-	-noFDR \
-	-verb
-
-}
-
-
-logpath=${out}${subject}/results/${glm}
-
-remlfit < /dev/null > ${logpath}/runtime.log 2>&1 &
+/usr/local/pkg/afni_18/3dREMLfit \
+-matrix ${dir_out}/X.xmat.1D \
+-input ${name_img_run1}" "${name_img_run2} \
+-Rvar ${dir_out}/stats_var_${subject}.nii.gz \
+-Rbuck ${dir_out}/stats_${subject}.nii.gz \
+-rwherr ${dir_out}/wherr_${subject}.nii.gz \
+-rerrts ${dir_out}/errts_${subject}.nii.gz \
+-GOFORIT \
+-fout \
+-tout \
+-nobout \
+-noFDR \
+-verb
 
 
 cd ${wd}
