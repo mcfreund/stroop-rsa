ff72343f8bb639cf0fccf2e9312f1bb45ad23514
mcfreund <m.freund@wustl.edu>
2021-02-08 11:35:17 -0600

    dmcc4854075->562345

---
+++ in/change_bad_subject_id.R
@@ -0,0 +1,19 @@
+source(here::here("code", "strings.R"))
+source(here::here("code", "packages.R"))
+
+
+
+## behavior-and-events_group
+
+behav <- fread(here("in", "behavior-and-events_group201902.csv"))
+behav[subj == "DMCC4854075"]$subj <- "562345"
+fwrite(behav, here("in", "behavior-and-events_group201902.csv"))
+
+## summary_group20
+
+subj_sum <- fread(here("in", "summary_group201902.csv"))
+subj_sum[subj == "DMCC4854075"]$subj <- "562345"
+fwrite(subj_sum, here("in", "summary_group201902.csv"))
+
+## glm subdirectories
+
