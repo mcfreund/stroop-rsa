f23c27459fc5434ca40f59cd84ce20d7f489097d
mcfreund <m.freund@wustl.edu>
2021-02-08 12:36:30 -0600

    first try

---
+++ code/glms/master_3dREMLfit_par.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+
+## get vars
+
+glm_names=(pro_bias_acc-only_fmriprep pro_bias_acc-only_fmriprep pro_allstim_acc-only_fmriprep)
+suffices=("" _runwise _runwise)
+#glms=(Cues CongruencySwitch ListLength Congruency fix-item)
+#tasks=(Axcpt Cuedts Stern Stroop Stroop)
+#suffices=(_shifted _shifted _shifted _shifted "")
+sessions=proactive
+subjects=132017
+#filename="/data/nil-external/ccp/freund/stroop-rsa/in/subjects.txt"
+#mapfile -t subjects < $filename
+runs=(1 2)
+encoding_dir=(AP PA)
+#hemis=(L R)
+
+## directories
+
+stimts=/data/nil-external/ccp/freund/stroop-rsa/glms/
+out=/data/nil-external/ccp/freund/stroop-rsa/glms/
+img=/data/nil-bluearc/ccp-hcp/DMCC_ALL_BACKUPS/HCP_SUBJECTS_BACKUPS/fMRIPrep_AFNI_ANALYSIS/
+scripts=/data/nil-external/ccp/freund/stroop-rsa/code/glms/
+
+
+## fit
+
+
+for subject in ${subjects[@]}; do
+
+	echo ${subject}
+	
+	for glm_i in ${!glm_names[@]}; do
+	
+		glm=${glm_names[$glm_i]}
+		#task=${tasks[$glm_i]}
+		suffix=${suffices[$glm_i]}
+		
+		echo ${glm}${suffix}
+
+		source 3dREMLfit${suffix}_par.sh
+
+	done
+
+	wait
+
+done
+
