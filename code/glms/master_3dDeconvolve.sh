e3075efde82233f9f46f1a73ac3554e45ace2e59
mcfreund <m.freund@wustl.edu>
2021-02-09 11:07:55 -0600

    run em all

--- code/glms/master_3dDeconvolve.sh
+++ code/glms/master_3dDeconvolve.sh
@@ -7,9 +7,9 @@ suffix_runwise=("" _runwise _runwise)
 #glm_names=(pro_bias_acc-only_fmriprep pro_allstim_acc-only_fmriprep)
 #suffix_runwise=(_runwise _runwise)
 
-#filename="/data/nil-external/ccp/freund/stroop-rsa/in/subjects.txt"
-#mapfile -t subjects < $filename
-subjects=107321
+filename="/data/nil-external/ccp/freund/stroop-rsa/in/subjects.txt"
+mapfile -t subjects < $filename
+#subjects=107321
 
 runs=(1 2)
 encoding_dir=(AP PA)
