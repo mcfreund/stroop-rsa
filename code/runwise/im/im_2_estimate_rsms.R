083826d2e4674db2d79c40f8321f8867936cebeb
mike freund <mfreundc@gmail.com>
2021-02-18 09:26:05 -0600

    reorg

--- code/runwise/im/im_2_estimate_rsms.R
+++ code/runwise/im/im_2_estimate_rsms.R
@@ -1,12 +1,6 @@
 ## about ----
 ## 
-## reads in afni images (beta estimates from GLM) into a list.
-## this list contains one element per subject per parcel per hemisphere.
-## given a  mask, similarity measures are then calculated, and saved as
-##  .RDS files to stroop-rsa/out/rsa/.
-## additionaly saved are mean values for conducting a univariate analysis.
-## 
-## mike freund, 2019-12-24
+## mike freund, 2021-02-17
 
 
 ## setup ----
@@ -16,15 +10,15 @@ source(here::here("code", "strings.R"))
 source(here::here("code", "read_atlases.R"))
 source(here::here("code", "read_masks.R"))
 
+
+glm.name <- "pro_bias_acc-only_fmriprep_im_run"
+
 ## functions
 
-read_betas <- function(.dir.glms, .regs) {
+read_betas <- function(.dir.glms, .regs = bias.items) {
+  # .dir.glms = dir.glms
   
-  image.betas <- array(
-    NA, 
-    dim = c(75, 90, 75, length(.regs), 2), 
-    dimnames = list(i = NULL, j = NULL, k = NULL, reg = .regs, run = 1:2)
-    )
+  image.betas <- list(NULL, NULL)
   
   for (run.i in 1:2) {
     # run.i = 1
@@ -32,38 +26,26 @@ read_betas <- function(.dir.glms, .regs) {
     fname.nii <- file.path(paste0(.dir.glms, run.i), paste0("stats_", fit.subjs[subj.i], "_run", run.i, ".nii.gz"))
     if (file.exists(fname.nii)) {
       ## dims of image.run.full [i, j, k, ???, regressor]
-      image.full <- oro.nifti::readNIfTI(fname.nii, reorient = FALSE)
+      # image.full <- oro.nifti::readNIfTI(fname.nii, reorient = FALSE)
+      
+      image.full <- RNifti::readNifti(fname.nii)
+      
     } else stop("file nonexistant! ", paste0(fname.nii))
     
-    brick.nums <- rep(NA, length(.regs))  ## + 1 for sustained
-    for (reg.i in .regs) {
-      # reg.i <- regs[1]
-      
-      image.label <- paste0(reg.i, "#0_Coef")
-      
-      brick.str <- mikeutils::afni("3dinfo", paste("-label2index", image.label, fname.nii))
-
-      ## for error checking
-      
-      has.error <- grepl("error", brick.str, ignore.case = TRUE)
-      if (any(has.error)) stop("error loading brick nums: ", paste0(fit.subjs[subj.i], " ", reg.i))
-      
-      ## to remove function call that is included in output (when is.local.session)
-      
-      brick.str <- brick.str[!grepl("3dinfo: AFNI version", brick.str)]
-      brick.num <- as.numeric(brick.str)
-      brick.nums[which(reg.i == .regs)] <- brick.num
-      
-    }
     
-    ## get brick numbers for regressors
+    ## get labels
     
-    if (any(is.na(brick.nums))) stop("brick nums equal zero! ", paste0(fit.subjs[subj.i]))
+    labs <- mikeutils::afni("3dinfo", paste("-label",  fname.nii))
+    labs <- unlist(strsplit(labs, "\\|"))
+    is.reg <- grepl(paste0(.regs, "#[0-9]_Coef", collapse = "|"), labs)
+    regs <- gsub("([a:Z]*)(#.*)", "\\1", labs[is.reg])
     
     ## put subset of images (only the relevant regressors) into one array
     
     ## image.betas with dims [i, j, k, regs]:
-    image.betas[, , , , run.i] <- image.full[, , , 1, brick.nums + 1]
+    
+    image.betas[[run.i]] <- image.full[, , , 1, is.reg]
+    dimnames(image.betas[[run.i]]) <- list(i = NULL, j = NULL, k = NULL, condition = regs)
     
   }
   
@@ -73,26 +55,15 @@ read_betas <- function(.dir.glms, .regs) {
 
 
 
-# distance_cv <- function(B1, B2, m, regressors) {
-#   # B1 = B[, , 1]; B2 = B[, , 2]
-#   
-#   D <- colSums(t(m %*% B1 * m %*% B2)) / ncol(B1)
-#   matrix(D, ncol = length(regressors), dimnames = list(.row = regressors, .col = regressors))
-#   
-# }
-
-
 ## paths, vars
 
 dir.analysis <- here::here("glms")
 files.dir.analysis <- list.files(dir.analysis, pattern = "stats_var", recursive = TRUE)  ## get fit.subjs
-files.dir.analysis <- files.dir.analysis[grep("pro_bias_acc-only_fmriprep_im_run[1-2]/", files.dir.analysis)]
+files.dir.analysis <- files.dir.analysis[grep(glm.name, files.dir.analysis)]
 fit.subjs <- unique(gsub("/results/.*", "", files.dir.analysis))
 
 ## regs will be used to pull out (via string match) the statistic from the afni brick;
 ## thus must have the reg.suffix
-regs <- bias.items
-n.regs <- length(regs)
 n.subj <- length(fit.subjs)
 n.bias.items <- length(bias.items)
 
@@ -102,7 +73,7 @@ n.bias.items <- length(bias.items)
 ## each iteration collates RSMs into a single array, and saves it as a single .rds file.
 
 for (set.i in sets.of.rois) {
-  # set.i = "mmp"
+  # set.i = "masks"
   
   ## get numbers and create storage objects
   
@@ -133,14 +104,14 @@ for (set.i in sets.of.rois) {
     
     ## get afni images
     
-    dir.glms <- file.path(dir.analysis, fit.subjs[subj.i], "results", "pro_bias_acc-only_fmriprep_run")
-    image.betas <- read_betas(dir.glms, regs)
+    dir.glms <- file.path(dir.analysis, fit.subjs[subj.i], "results", glm.name)
+    image.betas <- read_betas(dir.glms)  ## returns list, as arrays may have differing numbers of dimensions by run
     
     
     ## generate rsm for each roi
     
     for (roi.i in seq_len(n.roi)) {  ## careful: roi.i is an index for as.numeric(rois)
-      # roi.i <- 1
+      # roi.i <- 23
       
       ## get and apply mask for roi.i
       
@@ -150,20 +121,27 @@ for (set.i in sets.of.rois) {
         mask.i <- masks[[roi.i]] == 1
       }
       
-      roi.betas <- apply(image.betas, c("reg", "run"), function(.) .[mask.i])
+      roi.betas <- lapply(image.betas, function(x1) apply(x1, "condition", function(x2) x2[mask.i]))  ## dirty
+
+      ## estimate similarity matrix:
       
-      ## estimate similarity matrices:
+      R <- cor(roi.betas[[1]], roi.betas[[2]])  ## cross-run: rows = run1, cols = run2
       
-      B <- aperm(roi.betas, c(2, 1, 3))  
-      names(dimnames(B)) <- c("condition", "vertex", "run")
+      ## aggregate by condition over trials
       
-      B1 <- B[bias.items, , 1]
-      B2 <- B[bias.items, , 2]
+      A1 <- model.matrix(~ 0 + dimnames(R)[[1]])  ## build dummy/indicator matrices
+      A2 <- model.matrix(~ 0 + dimnames(R)[[2]])
       
-      D             <- distance_cv(B1, B2, cmat, bias.items)
-      # D       <- distance_cv(t(scale(t(B1))), t(scale(t(B2))), cmat, bias.items)
+      A1 <- sweep(A1, 2, colSums(A1), "/")  ## scale cols
+      A2 <- sweep(A2, 2, colSums(A2), "/")
       
-      rsarray[, , subj.i, roi.i] <- D
+      R_ave <- tanh(t(A1) %*% atanh(R) %*% A2)  ## average
+      
+      rownames(R_ave) <- gsub("dimnames(R)[[1]]", "", rownames(R_ave), fixed = TRUE)  ## remove weird prefix
+      colnames(R_ave) <- gsub("dimnames(R)[[2]]", "", colnames(R_ave), fixed = TRUE)
+    
+      
+      rsarray[, , subj.i, roi.i] <- R_ave[bias.items, bias.items]
 
     }
     
@@ -180,7 +158,7 @@ for (set.i in sets.of.rois) {
     rsarray, 
     here::here(
       "out", "rsa", "obsv", 
-      paste0("rsarray_pro_bias_acc-only_fmriprep_runwise_cv-euclidean_", set.i, ".rds")
+      paste0("rsarray_", glm.name, "_", set.i, ".rds")
       )
     )
   
---
+++ code/runwise/im/im_3_model_rdms.R
@@ -0,0 +1,125 @@
+## about ----
+## 
+## fits general linear models on each subject's RSMs from each parcel then writes the results to files.
+## 
+## mike freund, created 2019-02-24, updated 2019-03-05
+## adapted for new project directory 2019-12-29
+
+
+## setup ----
+
+source(here::here("code", "packages.R"))
+source(here("code", "strings.R"))
+source(here("code", "read_atlases.R"))
+source(here("code", "read_masks.R"))
+
+
+glm.name <- "pro_bias_acc-only_fmriprep_im_run"
+
+
+## analysis group:
+
+stroop <- fread(here("in", "behavior-and-events_group201902.csv"))
+sample.analysis <- unique(filter(stroop, is.analysis.group)$subj)
+
+n.dim <- length(bias.items)
+
+# subjs <- unique(stroop$subj)
+
+
+## read models
+
+rsv.models <- fread(here("out", "rsa", "mods", "rsv_bias_full-matrices.csv"), data.table = FALSE)
+
+## create design matrices
+
+rsv.models <- as.matrix(rsv.models[sapply(rsv.models, is.numeric)])
+rsv.models <- cbind(rsv.models, diagonal = c(diag(length(bias.items))))
+X <- scale(rsv.models)
+
+
+## loop over sets of ROIs ----
+
+for (set.i in sets.of.rois) {
+  # set.i = "masks"
+  
+  ## read observed similarity matrices (arrays)
+  
+  rsarray <- readRDS(
+    here(
+      "out", "rsa", "obsv",
+      paste0("rsarray_", glm.name, "_", set.i, "_imcomplete.rds")
+    )
+  )
+  
+  subjs <- dimnames(rsarray)$subj
+  
+  
+  ## prepare similarity matrices for regression ----
+  
+  ## check if rows and col names are equal (should be, but just to be sure...)
+  
+  are.rowcol.equal <- isTRUE(all.equal(dimnames(rsarray)[[1]], dimnames(rsarray)[[2]]))
+  if(!are.rowcol.equal) stop("rsarray isn't rsarray!")
+  
+  ## get indices and values
+  
+  rois <- dimnames(rsarray)$roi
+  n.mods <- length(subjs) * length(rois)
+  
+  ## unwrap into vector
+  
+  rsvectors <- vector("list", n.mods)
+  names(rsvectors) <- combo_paste(subjs, rois)
+  
+  for (subj.i in seq_along(subjs)) {
+    for (roi.j in seq_along(rois)) {
+      # subj.i = 1; roi.j = 1
+      
+      name.ij <- paste0(subjs[subj.i], "_", rois[roi.j])  ## to match name
+      rsvectors[[name.ij]] <- scale(rank(c(rsarray[, , subj.i, roi.j])))  ## z-score
+      
+    }
+  }
+  
+  ## check numbers
+  
+  lengths.rsvectors <- map_dbl(rsvectors, length)
+  if(sum(lengths.rsvectors != 256) > 0) stop("missing row somewhere in rsvectors!")
+  
+  
+  ## fit glms ----
+  
+  fits <- rsvectors %>% map(~ .lm.fit( x = X, y = .))
+  betas <- as.data.frame(do.call(rbind, lapply(fits, coef)))
+  
+  names(betas) <- colnames(X)
+  betas$id <- rownames(betas)
+  betas <- melt(as.data.table(betas), id.vars = "id", value.name = "beta", variable.name = "param")
+  
+  
+  ## format ----
+  
+  ## create subj, roi, and hemi cols from id col
+  
+  stats.subjs <- bind_cols(
+    betas,
+    reshape2::colsplit(betas$id, pattern = "_", names = c("subj", "roi"))
+  )
+  
+  stats.subjs$is.analysis.group <- stats.subjs$subj %in% sample.analysis  ## add is.analysis.group col
+  
+  ## rearrange cols (and drop id col)
+  
+  stats.subjs %<>% select(subj, is.analysis.group, roi, param, beta)
+  
+  ## write ----
+  
+  fwrite(
+    stats.subjs,
+    here("out", "rsa", "stats",  paste0("subjs", glm.name, "_", set.i, ".csv"))
+  )
+  
+  
+}
+
